14e51b3d0196b5e711078fadbfd16b615a9cb84a
linhgt <linhnhatmen@gmail.com>
2026-09-07 11:27:29 +0700

    feat: config tool chains

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    target: "es2022",
+    outDir: "dist",
+    sourcemap: "hidden",
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          return null;
+        },
+      },
+    },
+  },
+  server: {
+    port: 5173,
+  },
+  preview: {
+    port: 4173,
+  },
+});
